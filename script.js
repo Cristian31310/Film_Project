a94d1e498050539e13519698405b859762b047a9
Cristian31310 <cristian.martin.almendros@gmail.com>
2025-11-26 13:55:32 +0100

    Refactorizado codigo javascript

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
 console.log(peliculas);
 
-
 const container = document.createElement("div");
 container.style.display = "grid";
 container.style.gridTemplateColumns = "1fr 1fr 1fr";
 container.style.color = "white";
-for (let i = 0; i < peliculas.Search.length; i++) {
-    const div = document.createElement("div");
-    const caratula = document.createElement("img");
-    const title = document.createElement("p");
-    const year = document.createElement("p");
-    const type = document.createElement("p");
-    title.textContent = peliculas.Search[i].Title;
-    year.textContent = peliculas.Search[i].Year;
-    type.textContent = peliculas.Search[i].Type;
-    caratula.setAttribute("src", peliculas.Search[i].Poster);
-    caratula.style.height = "500px";
-    caratula.style.width = "340px";
-    caratula.style.margin = "25px";
-    div.style.textAlign = "center";
-    div.appendChild(caratula);
-    div.appendChild(title);
-    div.appendChild(year);
-    div.appendChild(type);
-    container.appendChild(div);
-    
+
+var div;
+var caratula;
+var title;
+var year;
+var type;
+
+function createElements() {
+  div = document.createElement("div");
+  caratula = document.createElement("img");
+  title = document.createElement("p");
+  year = document.createElement("p");
+  type = document.createElement("p");
 }
 
+function insertJsonContent(i) {
+  title.textContent = peliculas.Search[i].Title;
+  year.textContent = peliculas.Search[i].Year;
+  type.textContent = peliculas.Search[i].Type;
+  caratula.setAttribute("src", peliculas.Search[i].Poster);
+}
+
+function elementStyles() {
+  caratula.style.height = "500px";
+  caratula.style.width = "340px";
+  caratula.style.margin = "25px";
+  div.style.textAlign = "center";
+}
+
+function apendJsonElement() {
+  div.appendChild(caratula);
+  div.appendChild(title);
+  div.appendChild(year);
+  div.appendChild(type);
+  container.appendChild(div);
+}
+
+function bodyStyle() {
+  document.body.style.backgroundImage = "url(./img/spaceBG.jpg)";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  document.body.style.backgroundAttachment = "fixed";
+}
+
+function search() {}
+
+function displayFilms(dataFilms) {
+  for (let i = 0; i < dataFilms.Search.length; i++) {
+    createElements();
+    insertJsonContent(i);
+    elementStyles();
+    apendJsonElement();
+  }
+}
+
+displayFilms(peliculas);
+bodyStyle();
 document.body.appendChild(container);
-document.body.style.backgroundImage = "url(./img/spaceBG.jpg)";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "100%";
-document.body.style.backgroundSize = "100%";
-document.body.style.backgroundAttachment = "fixed";

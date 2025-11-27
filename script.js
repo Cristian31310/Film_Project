1e0d2b2dbe7d0b94181bbf7c95456bfb921632ed
Cristian31310 <cristian.martin.almendros@gmail.com>
2025-11-27 08:38:29 +0100

    Peliculas con buscador funcional

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const container = document.createElement("div");
+
+// const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=496cdeca", {});
+// const response = fetch("http://www.omdbapi.com/?apikey=496cdeca&s=Star").then;
+
+var userSearch = "pink+floyd";
+var apiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=" + userSearch;
+const buttonSearch = document.getElementById("searchButtom");
+
 container.style.display = "grid";
 container.style.gridTemplateColumns = "1fr 1fr 1fr";
 container.style.color = "white";
@@ -17,11 +25,11 @@ function createElements() {
   type = document.createElement("p");
 }
 
-function insertJsonContent(i) {
-  title.textContent = peliculas.Search[i].Title;
-  year.textContent = peliculas.Search[i].Year;
-  type.textContent = peliculas.Search[i].Type;
-  caratula.setAttribute("src", peliculas.Search[i].Poster);
+function insertJsonContent(data, i) {
+  title.textContent = data.Search[i].Title;
+  year.textContent = data.Search[i].Year;
+  type.textContent = data.Search[i].Type;
+  caratula.setAttribute("src", data.Search[i].Poster);
 }
 
 function elementStyles() {
@@ -41,24 +49,55 @@ function apendJsonElement() {
 
 function bodyStyle() {
   document.body.style.backgroundImage = "url(./img/spaceBG.jpg)";
-  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundRepeat = "repeat";
   document.body.style.backgroundSize = "100%";
   document.body.style.backgroundAttachment = "fixed";
 }
 
-function search() {
-    displayFilms(masPeliculas);
-}
+// function search() {
+//   displayFilms(masPeliculas);
+// }
 
 function displayFilms(dataFilms) {
   for (let i = 0; i < dataFilms.Search.length; i++) {
     createElements();
-    insertJsonContent(i);
+    insertJsonContent(dataFilms, i);
     elementStyles();
     apendJsonElement();
   }
 }
 
-displayFilms(peliculas);
+function movieSearch() {
+  fetch(apiURL)
+    .then((response) => response.json())
+    // .then((data) => console.log(data));
+    .then((data) => displayFilms(data));
+}
+
+// movieSearch();
+
+const form  = document.getElementById('form');
+
+movieSearch();
+form.addEventListener('submit', (event) => {
+   var algo = document.getElementById("movieInput").value
+
+});
+
+window.onscroll = (e) => {
+  let cercaFinal = window.innerHeight + window.scrollY >= document.body.offsetHeight - 300
+
+  if(cercaFinal){
+    console.log("saltaImagen");
+    
+  }
+  console.log("scroll");
+}
+
+// buttonSearch.addEventListener("click", search);
+
+// console.log(document.getElementById("movieInput").value);
+
+// displayFilms(peliculas);
 bodyStyle();
 document.body.appendChild(container);

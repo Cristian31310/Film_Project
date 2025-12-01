30faa77f3bab0f9a77c504a2b03557a12bec80f3
Cristian31310 <cristian.martin.almendros@gmail.com>
2025-12-01 22:14:13 +0100

    Se puede puscar tanto por pelicula como por serie

--- controller.js
+++ controller.js
@@ -4,11 +4,16 @@ var pageNumber = 1;
 var petecionEnCurso;
 
 var page = "&page=";
-var userSearch = "pink+floyd";
+var userSearch = "Godzilla";
 var apiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=";
-var defaultApiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=" + userSearch + page + pageNumber;
+var type = "movie"
+var defaultApiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=" + userSearch + page + pageNumber + "&type=" + type;
 
 class controller {
+
+  static setType(search) {
+    type = search;
+  }
 
   static setUserSearch(search) {
     userSearch = search;
@@ -19,14 +24,13 @@ class controller {
   }
 
   static movieSearch(container) {
-    defaultApiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=" + userSearch + page + pageNumber;
+    defaultApiURL = "http://www.omdbapi.com/?apikey=496cdeca&s=" + userSearch + page + pageNumber + "&type=" + type;
     if (!petecionEnCurso) {
       petecionEnCurso = true;
       fetch(defaultApiURL)
         .then((response) => response.json())
         .then((data) => view.displayFilms(data, container));
       petecionEnCurso = false;
-      console.log(defaultApiURL);
       pageNumber++;
     }
   }
--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 
-const buttonSearch = document.getElementById("searchButtom");
+const buttonSearchMovie = document.getElementById("buttonMovie");
+const buttonSearchSerie = document.getElementById("buttonSerie");
+
 var inputMovie = document.getElementById("movieInput");
+var inputSerie = document.getElementById("serieInput");
 var petecionEnCurso;
 
 var container = document.createElement("div");
 container.setAttribute("id", "superDiv");
 
 window.onload = () => {
+  searchButtom(buttonSearchMovie, inputMovie, "movie");
+  searchButtom(buttonSearchSerie, inputSerie, "series");
+}
 
+function searchButtom(button, input, type) {
   controller.movieSearch(container);
 
-  buttonSearch.addEventListener("click", (event) => {
-    if (controller.userSearch != inputMovie.value) {
+  button.addEventListener("click", (event) => {
+    console.log("INPUT =>" + input.value)
+    if (controller.userSearch != input.value) {
       controller.setPageNumber(1);
+      controller.setType(type)
       view.reload();
     }
 
-    controller.setUserSearch(inputMovie.value);
+    controller.setUserSearch(input.value);
     controller.movieSearch(container);
   });
 }
-
 
 window.onscroll = (e) => {
   let cercaFinal =
